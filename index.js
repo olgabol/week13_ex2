eb08b7b8db319129b82b0ff2e5ec5a3cb4740e0c
olgabol <olgabolelova@gmail.com>
2022-05-01 19:33:11 +0300

    new commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function showSumm () {
+    let a = prompt ('Первое число?');
+    let b = prompt ('Второе число?');
+    alert (+a + +b);
+
+};
+
+function showSubtr () { 
+    let a = prompt ('Первое число?');
+    let b = prompt ('Второе число?');
+    alert (a - b);
+
+};
+
+function showMult () {
+    let a = prompt ('Первое число?');
+    let b = prompt ('Второе число?');
+    alert (a * b);
+};
+
+function showDevis () {
+    let a = prompt ('Первое число?');
+    let b = prompt ('Второе число?');
+    alert (a / b);
+};
